d86a5baa7eefbf9f596545b79613987001b81b2c
mogataro <negishikouhei1013@gmail.com>
2019-03-13 03:55:08 +0900

    Fix 認証設定

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,22 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// --------------------------
+//  認証関連
+// --------------------------
+// アプリケーションでユーザー登録が必要なければ、新しく作成されたRegisterControllerを削除し、
+// ルート定義をAuth::routes(['register' => false]);のように変更すれば、無効にできます。
+Auth::routes();
+
+// 認証されている時、アクセス可能
+Route::group(['middleware' => 'auth'], function() {
+
+    Route::get('/home', 'HomeController@index')->name('home');
+
+    // 認証されており、かつ、管理者権限のユーザーの時、アクセス可能
+    Route::group(['middleware' => 'administrator'], function() {
+        Route::get('/admin', 'HomeController@test')->name('admin');
+    });
+});
