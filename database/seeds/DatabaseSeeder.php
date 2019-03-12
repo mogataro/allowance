700d0bbe4933ed1c4bb3e59ccafe5af363b11013
mogataro <negishikouhei1013@gmail.com>
2019-03-13 02:16:10 +0900

    Fix DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -6,11 +6,16 @@ class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
-     *
+     * @author negishi
+     * @since  2019-03-13
+     * 
      * @return void
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call([
+            UserAuthoritiesTableSeeder::class,      // ユーザー権限テーブル用
+            UsersTableSeeder::class                 // ユーザーテーブル用
+        ]);
     }
 }
