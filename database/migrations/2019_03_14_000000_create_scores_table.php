b1c41137ed375bad489a88ad78812d452a9c53eb
mogataro <negishikouhei1013@gmail.com>
2019-03-13 23:18:53 +0900

    Fix スコアテーブルマイグレーションクラス修正

--- database/migrations/2019_03_14_000000_create_scores_table.php
+++ database/migrations/2019_03_14_000000_create_scores_table.php
@@ -17,6 +17,13 @@ class CreateScoresTable extends Migration
     {
         Schema::create('scores', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->char('imp_year',4);//実施年度(西暦)
+            $table->char('imp_month',2);//実施月
+            $table->string('test_type');//テスト大種別(定期テスト,模擬テストなど)
+            $table->string('test_class');//テスト小項目(大項目と親子関係必要：中間テスト,期末テスト,第2回代ゼミスタンダードレベル模試など)
+            $table->string('subjects');//科目(算数、国語など)
+            $table->integer('acquisition_score')->unsigned()->nullable($value = true);//取得点数
+            $table->integer('perfect_score')->unsigned();//満点
             $table->timestamps();
         });
     }
