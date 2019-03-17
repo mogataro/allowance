a9927ba8afcb3385388ad82611a604130564ab46
mogataro <negishikouhei1013@gmail.com>
2019-03-17 19:11:44 +0900

    Add Test関係テーブルseeder追加

---
+++ database/seeds/TestSubjectsTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TestSubjectsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
