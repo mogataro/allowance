51b7e10347522667a6409bbb7a3b5ac0be08103b
mogataro <negishikouhei1013@gmail.com>
2019-03-22 10:47:08 +0900

    Fix modelリレーション追加

--- app/Model/TestScore.php
+++ app/Model/TestScore.php
@@ -30,4 +30,24 @@ class TestScore extends Model
     {
         return $this->belongsTo('App\Model\TestType', 'test_type_id', 'id');
     }
+
+    /**
+     * リレーション (多対1の関係)
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function testClass() // 単数形
+    {
+        return $this->belongsTo('App\Model\TestClass', 'test_class_id', 'id');
+    }
+
+    /**
+     * リレーション (多対1の関係)
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function testSubject() // 単数形
+    {
+        return $this->belongsTo('App\Model\TestClass', 'test_subject_id', 'id');
+    }
 }
